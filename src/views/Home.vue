d08893c9c56c8af9a52f871dd74f3f332bab8d9c
oleg086rus <o.pototskiy@yandex.ru>
2021-01-03 01:58:11 +0500

    vue-practice

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae placeat aperiam voluptates, nobis corporis magnam laudantium nisi corrupti, ullam libero illum ad omnis? Voluptate maxime rem facere, deserunt ipsam dicta.
+    </p>
+    <router-link to="/todos">todos</router-link>
+  </div>
+</template>
